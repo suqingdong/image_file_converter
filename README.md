6ca77ede44ca7800c8705d678393002fd3869808
suqingdong <1078595229@qq.com>
2024-10-24 17:44:26 +0800

    add build

--- README.md
+++ README.md
@@ -1,21 +1,57 @@
-# Convert Image File to DataURL
+# Toolkit for converting image files
 
 ## Installation
 
 ```bash
-python -m pip install image-to-dataurl
+python -m pip install image_file_converter
 ```
 
-## Usage
+## Usage in Python
 
 ```python
-from image_to_dataurl import image_to_dataurl
+from image_file_converter.core import ImageFileConverter
 
-# support image file or pdf file
-dataurl = image_to_dataurl('/path/to/image.png')
-dataurl = image_to_dataurl('/path/to/image.jpg')
-dataurl = image_to_dataurl('/path/to/image.pdf')
+# convert pdf to image or data_url
+input_file = 'tests/test.pdf
+converter = ImageFileConverter(input_file, dpi=300, max_page=5)
+converter.to_image('output.png')
+converter.to_image('output.jpg')
+data_url = converter.to_data_url()
+print(data_url)
 
+# convert image to pdf or data_url
+input_file = 'tests/test.png'
+converter = ImageFileConverter(input_file)
+converter.to_pdf('output.pdf')
+data_url = converter.to_data_url()
+print(data_url)
 ```
 
+## Usage in Jupyter Notebook
 
+```python
+from image_file_converter.core import ImageFileConverter
+
+# convert pdf to image or data_url
+input_file = 'tests/test.pdf'
+```
+
+## Usage in CMD
+
+```bash
+image_file_converter --help
+image_file_converter --version
+
+# convert pdf to image
+image_file_converter to_image tests/test.pdf -o output.png
+image_file_converter to_image tests/test.pdf -o output.jpg
+image_file_converter to_image tests/test.pdf -o output.png --dpi 300 --max-page 5
+
+# convert image to pdf
+image_file_converter to_pdf tests/test.png -o output.pdf
+
+# convert image or pdf to data_url
+image_file_converter to_data_url tests/test.png
+image_file_converter to_data_url tests/test.pdf
+image_file_converter to_data_url tests/test.pdf --dpi 300 --max-page 5
+```
--- image_file_converter/bin.py
+++ image_file_converter/bin.py
@@ -4,13 +4,35 @@ from image_file_converter import version_info
 from image_file_converter.core import ImageFileConverter
 
 
-CONTEXT_SETTINGS = dict(help_option_names=['-?', '-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=['-?', '-h', '--help'], max_content_width=200)
+
+__epilog__ = '''
+\n\b
+examples:
+    # 1. convert pdf to image
+    image_file_converter to_image tests/test.pdf -o output.png
+    image_file_converter to_image tests/test.pdf -o output.jpg
+    image_file_converter to_image tests/test.pdf -o output.png --dpi 300 --max-page 5
+\b
+    # 2. convert image to pdf
+    image_file_converter to_pdf tests/test.png -o output.pdf
+    image_file_converter to_pdf tests/test.jpg -o output.pdf
+\b
+    # 3. convert image or pdf to data_url
+    image_file_converter to_data_url tests/test.png
+    image_file_converter to_data_url tests/test.pdf
+    image_file_converter to_data_url tests/test.pdf --dpi 300 --max-page 5
+\x1b[1;30m
+
+contact: {author}<{author_email}>
+'''.format(**version_info)
 
 @click.command(
     name=version_info['prog'],
     help=click.style(version_info['desc'], italic=True, fg='cyan', bold=True),
     context_settings=CONTEXT_SETTINGS,
     no_args_is_help=True,
+    epilog=click.style(__epilog__, fg='yellow'),
 )
 @click.version_option(version=version_info['version'], prog_name=version_info['prog'])
 @click.argument('command', type=click.Choice(['to_image', 'to_pdf', 'to_data_url']))
@@ -18,7 +40,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-?', '-h', '--help'])
 @click.option('-o', '--output-file', help='the output file name')
 @click.option('--dpi', help='the dpi of pdf to image converter', type=int, default=300, show_default=True)
 @click.option('--max-page', help='the max page of pdf to image converter', type=int)
-def main(command, input_file, output_file, dpi, max_page):
+def cli(command, input_file, output_file, dpi, max_page):
     converter = ImageFileConverter(input_file, dpi=dpi, max_page=max_page)
     if command != 'to_data_url' and output_file is None:
         suffix = 'pdf' if command == 'to_pdf' else 'png'
@@ -34,6 +56,9 @@ def main(command, input_file, output_file, dpi, max_page):
     if output_file is not None:
         click.secho(f'save file: {output_file}', err=True, fg='green')
 
+
+def main():
+    cli()
 
 if __name__ == '__main__':
     main()
